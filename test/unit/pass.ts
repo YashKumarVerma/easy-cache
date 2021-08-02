96470de490c522e4a39c95da9fac589fa5ea9193
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-08-02 23:48:56 +0530

    test(setup): migrate to TS for tests

---
+++ test/unit/pass.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+describe('Testing Environment', () => {
+  it('should pass if setup correctly', () => {
+    expect(true).to.equal(true)
+  })
+})
