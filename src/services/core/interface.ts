c3a6df5500d6e3148bbe5a9977ab4f4c801137ef
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-08-02 06:51:28 +0530

    feat(core) : decorator config object

--- src/services/core/interface.ts
+++ src/services/core/interface.ts
@@ -1,4 +1,7 @@
-export interface ConfigurationObject {
+/**
+ * @constructor parameters to customize the behavior of the service
+ */
+export interface ConstructorConfig {
   /** redis backend port */
   redisPort: number
 
@@ -12,5 +15,51 @@ export interface ConfigurationObject {
   defaultTTL: number
 
   /** debug mode enables verbose logging */
+  debug?: boolean
+
+  /** to disable the cache */
+  disable?: boolean
+}
+
+/**
+ * @Cache decorator parameters to customize runtime behavior
+ */
+export interface CacheDecoratorConfig {
+  /**
+   * setting it true will disable cachine for the given function.
+   *
+   * This has been provided to allow the cahce to be disabled for specific
+   * functions programatically. This is useful for testing purposes,
+   * and also to allow for the cache to be disabled based on a condition
+   * or environment configuration.
+   *
+   * @default false
+   */
+  disable?: boolean
+
+  /**
+   * expireAfter is an alias for TTL. The cache of the given function will
+   * automatically be invalidated after the given number of milliseconds.
+   *
+   * Setting it to 0 will preserve the cache for the given function forever.
+   * If this is not provided, then the default TTL defined in the constructor
+   * will be used.
+   * @default 0
+   */
+  expireAfter?: number
+
+  /**
+   * setting it true will log all operations performed by the module for the
+   * given function. This is mainly for debugging purposes.
+   *
+   * If this is not set, then the debug flag defined in the constructor will
+   * be used.
+   *
+   * If both the flags contradict, then this would be given preference. i.e.
+   * If debug mode is off for the entire application, and debug:true is set
+   * the debug mode for the given function will be operational.
+   *
+   * @default false
+   */
   debug?: boolean
 }
