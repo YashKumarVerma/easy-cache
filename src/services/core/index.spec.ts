51cefd9aa162324b16dfcd23da49d2839848aeaa
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-08-05 02:08:26 +0530

    test(core): test @directive,  cache checks

--- src/services/core/index.spec.ts
+++ src/services/core/index.spec.ts
@@ -8,13 +8,18 @@ import { describe, it } from 'mocha'
 import EasyCache from './index'
 import { ConstructorConfig } from './interface'
 import { InsufficientDataException } from '../error/insufficientData'
+import {
+  currentTimeStampInSeconds,
+  secondsFromTimestamp,
+  sleep,
+} from '../util/time'
 
 /**
  * @constants
  * Congiruations to run the tests on
  */
 const ENV: ConstructorConfig = {
-  defaultTTL: 2,
+  defaultTTL: 5,
   redisHost: 'localhost',
   redisPassword: '',
   redisPort: 6379,
@@ -192,6 +197,170 @@ describe('core/EasyCache', () => {
         'args2',
       ])
       expect(signature1).not.to.equal(signature2)
+    })
+  })
+
+  /**
+   * Test cases for @Cache
+   */
+  describe('@Cache', () => {
+    /** should be defined */
+    it('should be defined', () => {
+      const EasyCacheInstance = new EasyCache(ENV)
+      expect(EasyCacheInstance.Cache).not.to.be.undefined
+    })
+
+    /** should be a directive */
+    it('should be a directive', () => {
+      const EasyCacheInstance = new EasyCache(ENV)
+      expect(EasyCacheInstance.Cache).to.be.a('function')
+    })
+
+    /** directives should always return a function */
+    it('should execute the host function with expected output', async () => {
+      /** declare an instance of EasyCache */
+      const EasyCacheInstance = new EasyCache({
+        ...ENV,
+        defaultTTL: 10,
+      })
+
+      class DummyClass {
+        @EasyCacheInstance.Cache({})
+        public static host() {
+          return true
+        }
+      }
+
+      const hostOutput = await DummyClass.host()
+      expect(DummyClass.host).to.be.a('function')
+      expect(hostOutput).to.be.true
+    })
+
+    /**
+     * @important
+     *
+     * Since this is the first version of the package, with support for only
+     * one plugin, i.e. RedisPlugin (which is the default plugin right now),
+     * This testcase is written as a unit test for the code module.
+     *
+     * In due time, this testcase will be migrated to e2e sute of RedisPlugin
+     * and removed from here.
+     *
+     * @todo: migrate to e2e test of RedisPlugin
+     */
+    /** should transform host function to async */
+    it('[redisPlugin] should transform host function to async', async () => {
+      /** declare an instance of EasyCache */
+      const EasyCacheInstance = new EasyCache(ENV)
+
+      class DummyClass {
+        @EasyCacheInstance.Cache({})
+        public static host() {
+          return true
+        }
+      }
+
+      expect(DummyClass.host).to.be.a('function')
+      expect(DummyClass.host()).not.to.be.true
+    })
+
+    /**
+     * Test Cache Functionality
+     */
+    /** check if cache is working */
+    it('should cache the output of the host function [time based check]', async () => {
+      /** declare an instance of EasyCache */
+      const EasyCacheInstance = new EasyCache(ENV)
+
+      class DummyClass {
+        /** this function resolves after 1 second to run */
+        static async someBulkyOperation(): Promise<string> {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              resolve('resultOfSomeBulkyOperation')
+            }, 1000)
+          })
+        }
+
+        /**
+         * this is the function that is being cached
+         */
+        @EasyCacheInstance.Cache({
+          expireAfter: 2,
+        })
+        static async cachedFunction() {
+          const result = await DummyClass.someBulkyOperation()
+          return result
+        }
+
+        static async normalFunction() {
+          const result = await DummyClass.someBulkyOperation()
+          return result
+        }
+
+        static async test(enableCache = false): Promise<Array<string>> {
+          const results: Array<string> = []
+
+          for (let i = 0; i < 5; i += 1) {
+            if (enableCache) {
+              const result = await DummyClass.cachedFunction()
+              results.push(result)
+            } else {
+              const result = await DummyClass.normalFunction()
+              results.push(result)
+            }
+          }
+
+          return results
+        }
+      }
+
+      /**
+       * now run the function a bunch of times, say 10 times
+       * Only the output of the first run should take 1000ms to run.
+       * Subsequent runs should be way faster than 1000ms.
+       */
+
+      /** run the function 10 times, without caching it should take ~5 seconds */
+      const initialTimeStamp = currentTimeStampInSeconds()
+      const results = await DummyClass.test()
+      const timeTakenInSeconds = secondsFromTimestamp(initialTimeStamp)
+
+      /**
+       * Since the loop runs 5 times, the non cached function should take about
+       * 1000 * 5 = 5000ms to run.
+       *
+       * 500ms are taken for timestamp irregularity.
+       */
+      expect(timeTakenInSeconds).to.be.above(4.5)
+      expect(timeTakenInSeconds).to.be.below(5.5)
+      expect(results.length).to.be.equal(5)
+
+      /** Now run the function 5 times with caching enabled, it should take ~1 second */
+      const initialTimeStampWhenCached = currentTimeStampInSeconds()
+      const resultsWhenCached = await DummyClass.test(true)
+      const timeTakenInSecondsWhenCached = secondsFromTimestamp(
+        initialTimeStampWhenCached,
+      )
+
+      expect(resultsWhenCached.length).to.be.equal(5)
+      expect(timeTakenInSecondsWhenCached).to.be.above(0.5)
+      expect(timeTakenInSecondsWhenCached).to.be.below(1.5)
+
+      console.table([
+        {
+          cached: false,
+          start: Math.floor(initialTimeStamp),
+          time: `${Math.floor(timeTakenInSeconds)} seconds`,
+          timesRun: results.length,
+        },
+        {
+          cached: true,
+          start: Math.floor(initialTimeStampWhenCached),
+          time: `${Math.floor(timeTakenInSecondsWhenCached)} seconds`,
+          timesRun: resultsWhenCached.length,
+        },
+      ])
     })
   })
 })
