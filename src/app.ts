75ff12fd0bdc90d5aebc9d6f469b1793829269c5
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-08-05 02:16:41 +0530

    fix(demo): add config params

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import Package from './index'
 
 const pkg = new Package({
- 
+  defaultTTL: 5,
+  redisHost: '127.0.0.1',
+  redisPort: 6379,
+  redisPassword: '',
+  debug: false,
 })
 
 console.log(pkg)
