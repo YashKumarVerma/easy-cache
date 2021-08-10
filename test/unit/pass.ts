afe1e9a4c99a1444bb2222175fca01df1d65f469
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-08-10 08:20:14 +0530

    build(npm): configure import path

--- test/unit/pass.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { expect } from 'chai'
-import { describe, it } from 'mocha'
-
-describe('Testing Environment', () => {
-  it('should pass if setup correctly', () => {
-    expect(true).to.equal(true)
-  })
-})
